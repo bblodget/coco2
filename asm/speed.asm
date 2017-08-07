f5bdd99184e3e0e52b7187b470c42419aa39981e
Brandon Blodget <brandon@shadowboxtech.com>
2017-08-06 20:58:08 -0700

    New speed.asm. Rewrite of speed.bas in assembly.

---
+++ asm/speed.asm
@@ -0,0 +1,16 @@
+; This is a speed test
+; It fills the screen 
+; with "A" then "B" etc
+; to "Z" then stops
+
+start   lda     #64        start at "A"-1
+inclet  inca               next letter
+        ldx     #$400      point to screen
+nxtpos  sta     ,x+        put letter on screen
+        cmpx    #$400+511  test for screen end
+        bne     nxtpos     loop until last pos
+        cmpa    #90        test with Z
+        bne     inclet     get the next letter
+loop    jmp     loop        loop here
+        end
+
